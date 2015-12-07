b35d9348d26a4ebeda110f954065a98712f51498
Jisu Park <jpark06@vt.edu>
2015-12-06 22:01:31 -0500

    Draft: User Interaction

--- hangman.py
+++ hangman.py
@@ -3,9 +3,9 @@
 #Jisu Park
 
 #instruction
+print " "
 print "Hangman Game!!"
 print "Guess the correct word in 10 tries"
-print " "
 
 #variables
 tries = 10
@@ -13,11 +13,49 @@ words = ["pineapple", "banana", "strawberry", "watermelon", "blueberry",
 	 "pear", "orange", "cherry", "pomegranate", "cantaloupe"]
 import random
 randNum = random.randint(0,9)
-myWord = words[randNum]
+myWord = list(words[randNum])
 count = len(myWord)
+correctGuess = 0
 answer = ['_'] * count
 
 while(tries > 0):
-	print "Tries: %d" % tries
+	print " "
+	print "Tries Left: %d" % tries
 	print ' '.join(answer)
+	print " "
+	guess = raw_input("Guess: ")
+
+	correct = False
+	for i in range(0, count-1):
+		if guess == myWord[i]:
+			answer[i] = guess
+			correctGuess = correctGuess + 1		
+			correct = True
+	#check whether correct letter was guessed 
+	if correct == True:
+		print "Correct Guess :)"
+		print " "
+	else: 
+		tries = tries - 1
+		print "Incorrect Guess :("
+		print " "
+	#check whether correct word was guessed
+	if correctGuess == count: 
+		tries = -1
+
+#print results
+if tries == 0:
+	print " "
+	print " "
+	print "YOU LOST :( THE ANSWER WAS %s" % myWord
+elif tries == -1:
+	print " "
+	print " "
+	print "CONGRATULATIONS :) YOU GUESSED THE CORRECT ANSWER"
+
+
+
+
+
+
 
