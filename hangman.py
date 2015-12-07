d1e4f1f9423aa797ceb0fbb329b105979bc5b362
Jisu Park <jpark06@vt.edu>
2015-12-06 21:24:25 -0500

    Draft: Game Setup

--- hangman.py
+++ hangman.py
@@ -1,1 +1,23 @@
-print "Hangman Game"
+#ECE2524 Project3
+#Hangman Game
+#Jisu Park
+
+#instruction
+print "Hangman Game!!"
+print "Guess the correct word in 10 tries"
+print " "
+
+#variables
+tries = 10
+words = ["pineapple", "banana", "strawberry", "watermelon", "blueberry",
+	 "pear", "orange", "cherry", "pomegranate", "cantaloupe"]
+import random
+randNum = random.randint(0,9)
+myWord = words[randNum]
+count = len(myWord)
+answer = ['_'] * count
+
+while(tries > 0):
+	print "Tries: %d" % tries
+	print ' '.join(answer)
+
